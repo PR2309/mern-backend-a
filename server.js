fc24ecf0e3006ac5c5e52faf6928d9ef356a628f
PR2309 <piyushanand2309@gmail.com>
2025-07-13 10:57:05 +0530

    Routes added

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ import userRouter from './Routes/userRoute.js'; // userRoute is a variable in th
 dotenv.config(); // loads environment variables from .env file
 
 const app=express();
+
 app.use(cors());
+app.use(express.json());
+app.use(express.static('public')); // serves static files from public folder
+
 const PORT=process.argv[2] || 8080;
 // const dbuser=encodeURIComponent(process.env.DBUSER);
 // const dbpass=encodeURIComponent(process.env.DBPASS);
@@ -30,23 +34,21 @@ const merndb = "merndb"; //DB NAME
 //     });
 // });
 
-// mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.db4tdjc.mongodb.net/${merndb}?retryWrites=true&w=majority&appName=Cluster0`) // for MongoDB Atlas
-// .then( () =>{ // returns a promise
-//     app.listen(PORT,()=>{ // first connect to database then run the server
-//         console.log(`Server is running live at vercel`);
-//     });
-// });
-
-// For Testing
-mongoose.connect(`mongodb://localhost:27017/mernCafe`) // for MongoDB Atlas
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.db4tdjc.mongodb.net/${merndb}?retryWrites=true&w=majority&appName=Cluster0`) // for MongoDB Atlas
 .then( () =>{ // returns a promise
-    app.listen(8080,()=>{ // first connect to database then run the server
-        console.log(`Server is running at http://localhost:8080`);
+    app.listen(PORT,()=>{ // first connect to database then run the server
+        console.log(`Server is running live at vercel`);
     });
 });
 
-app.use(express.json());
-app.use(express.static('public')); // serves static files from public folder
+// // For Testing
+// mongoose.connect(`mongodb://localhost:27017/mernCafe`) // for MongoDB Atlas
+// .then( () =>{ // returns a promise
+//     app.listen(8080,()=>{ // first connect to database then run the server
+//         console.log(`Server is running at http://localhost:8080`);
+//     });
+// });
+
 
 // connection checking
 app.get("/",(req,res)=>{
